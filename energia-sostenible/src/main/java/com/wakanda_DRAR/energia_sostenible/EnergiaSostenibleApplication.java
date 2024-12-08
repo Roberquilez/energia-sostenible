613e0013a1eda228001ce73cf39c498fab07e4bf
Roberquilez <159033774+Roberquilez@users.noreply.github.com>
2024-12-08 18:47:25 +0100

    Initial commit

---
+++ energia-sostenible/src/main/java/com/wakanda_DRAR/energia_sostenible/EnergiaSostenibleApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda_DRAR.energia_sostenible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergiaSostenibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergiaSostenibleApplication.class, args);
+	}
+
+}
---
+++ energia-sostenible/src/test/java/com/wakanda_DRAR/energia_sostenible/EnergiaSostenibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakanda_DRAR.energia_sostenible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergiaSostenibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
